ba84606d1f9fd09cc76c95cf8dece15eb1f98c79
nafil <tech.nafil@gmail.com>
2022-03-18 01:27:23 +0530

    initial app

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './Home';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Cuisine from './Cuisine';
+import Search from './Search';
+import Recipe from './Recipe';
+import { AnimatePresence } from 'framer-motion';
+
+function Pages() {
+  const location = useLocation();
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Routes Location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Search />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default Pages;
---
+++ src/pages/Search.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Search() {
+  const [searchedRecipies, setsearchedRecipies] = useState([]);
+  let params = useParams();
+
+  const getSearched = async (name) => {
+    const data = await fetch(`
+                https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&query=${name}
+            `);
+    const recipes = await data.json();
+    setsearchedRecipies(recipes.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  return (
+    <Grid>
+      {searchedRecipies.map((item) => {
+        return (
+          <Card key={item.id}>
+            <Link to={'/recipe/' + item.id}>
+              <img src={item.image} alt={item.title} />
+              <h4>{item.title}</h4>
+            </Link>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  h4 {
+    text-align: center;
+    padding: 1 rem;
+  }
+`;
+
+export default Search;
